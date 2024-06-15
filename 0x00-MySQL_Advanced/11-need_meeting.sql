8996bc1eeea0d18328c9d1134b741c77f0bac8ba
Muhadev <muhammedfayemi@gmail.com>
2024-06-15 21:54:48 +0100

    task is added

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- Create the view need_meeting
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE (score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)));
