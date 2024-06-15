e18ce02e405733d0f170b26836c126525d87b3c9
Muhadev <muhammedfayemi@gmail.com>
2024-06-15 21:39:26 +0100

    task is added

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,5 @@
--- Create an index on the first letter of name and score
+-- Ensure the index does not already exist
+DROP INDEX IF EXISTS idx_name_first_score ON names;
+
+-- Create the index on the first letter of name and score
 CREATE INDEX idx_name_first_score ON names (SUBSTRING(name, 1, 1), score);
