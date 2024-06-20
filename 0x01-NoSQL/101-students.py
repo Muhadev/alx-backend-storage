3532c6b290bcbc1fcb395310b9414144a5d78b5f
Muhadev <muhammedfayemi@gmail.com>
2024-06-20 10:20:34 +0100

    task is added

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python3
+'''Task 14's module.
+'''
+
+
 def top_students(mongo_collection):
-    """
-    Returns all students sorted by average score.
-    """
-    # Pipeline for the aggregation
-    pipeline = [
-        {
-            '$project': {
-                'name': 1,
-                'averageScore': {'$avg': '$topics.score'}
-            }
-        },
-        {
-            '$sort': {'averageScore': -1}
-        }
-    ]
-    # Perform the aggregation
-    return list(mongo_collection.aggregate(pipeline))
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
