5520496aa25264b4be0e807d2e402c4492241daf
Muhadev <muhammedfayemi@gmail.com>
2024-06-20 09:32:49 +0100

    task is added

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,35 +1,30 @@
 #!/usr/bin/env python3
-"""
-Provides statistics about Nginx logs stored in MongoDB.
-"""
-
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
-def log_stats():
-    """
-    Prints statistics about Nginx logs stored in MongoDB.
-    """
-    # Connect to the MongoDB server
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
 
-    # Get the total number of logs
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    # Define the HTTP methods to count
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    # Print the count of each HTTP method
-    print("Methods:")
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
 
-    # Count the number of documents with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
 
-if __name__ == "__main__":
-    log_stats()
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
